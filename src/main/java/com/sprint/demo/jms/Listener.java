d3522646925c27853ad35a3d6f9be5788dfb8d88
Deepthi Kota <deepthi.kota@sprint.com>
2018-06-29 15:45:09 -0500

    Update Listener.java

--- src/main/java/com/sprint/demo/jms/Listener.java
+++ src/main/java/com/sprint/demo/jms/Listener.java
@@ -4,8 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.TextMessage;
 
 import org.apache.activemq.command.ActiveMQBytesMessage;
 import org.apache.activemq.util.ByteSequence;
@@ -26,93 +24,60 @@ import com.sprint.demo.services.OrderRepository;
 
 @Component
 public class Listener {
-	 @Autowired
-		private  OrderRepository repository;
-	 @Autowired
-	 ConfirmOrderServiceImpl confirmOrderServiceImpl;
-	 
-	 @Autowired
-	  private JmsConfig jmsConfig;
-	 
-	 @Autowired
-	    private SimpMessagingTemplate messagingTemplate;
-		
-	
+	@Autowired
+	private OrderRepository repository;
+	@Autowired
+	ConfirmOrderServiceImpl confirmOrderServiceImpl;
+
+	@Autowired
+	private JmsConfig jmsConfig;
+
+	@Autowired
+	private SimpMessagingTemplate messagingTemplate;
+
+	@Autowired
+	private MessagePostProcessorImpl messagePostProcessorImplObj;
+
 	@JmsListener(destination = "order-details-queue")
-	public Map<String, ConfirmOrderLVOResponse> receiveMessage(final ActiveMQBytesMessage jsonMessage) throws JMSException {
+	@SendTo("processed.queue")
+	public Map<String, ConfirmOrderLVOResponse> receiveMessage(final ActiveMQBytesMessage jsonMessage)
+			throws JMSException {
 		System.out.println("Received message " + jsonMessage);
 		String response = null;
 		ByteSequence b = jsonMessage.getContent();
 		byte[] b1 = b.getData();
-		System.out.println("dsaadf : "+b1.toString());
-		String str = new String (b1);
-		 ConfirmOrderLVOResponse confirmOrderLVOResponse = null; 
-			ChatMessage orderDetailsObj1 = new Gson().fromJson(str, ChatMessage.class);
-			OrderDetails orderDetailsObj = new Gson().fromJson(orderDetailsObj1.getContent(), OrderDetails.class);
-			 //CALLING MULE API
-			 try {
-				 confirmOrderLVOResponse = confirmOrderServiceImpl.confirmOrder(orderDetailsObj);
-				 if(confirmOrderLVOResponse.getStatus() == 200){
-					 	confirmOrderLVOResponse.setOrderId(orderDetailsObj.getOrderId());
-						repository.deleteByOrderId(orderDetailsObj.getOrderId());
-					 	
-				 }else{
-					 confirmOrderLVOResponse.setOrderId(orderDetailsObj.getOrderId());
-					 orderDetailsObj.setHttpStatus(confirmOrderLVOResponse.getStatus());
-					 	orderDetailsObj.setErrorCode(confirmOrderLVOResponse.getErrorCode());
-					 	orderDetailsObj.setErrorCategory(confirmOrderLVOResponse.getCategory());
-					 	orderDetailsObj.setErrorMessage(confirmOrderLVOResponse.getErrorMessage());
-					 	repository.save(orderDetailsObj);
-				 }
-				 String s = "/topic/order-details"+orderDetailsObj1.getSender();
-						 messagingTemplate.convertAndSend(s, orderDetailsObj);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		System.out.println("dsaadf : " + b1.toString());
+		String str = new String(b1);
+		ConfirmOrderLVOResponse confirmOrderLVOResponse = null;
+		ChatMessage orderDetailsObj1 = new Gson().fromJson(str, ChatMessage.class);
+		OrderDetails orderDetailsObj = orderDetailsObj1.getOrderDetails();
+		// CALLING MULE API
+		try {
+			confirmOrderLVOResponse = confirmOrderServiceImpl.confirmOrder(orderDetailsObj);
+			if (confirmOrderLVOResponse.getStatus() == 200) {
+				confirmOrderLVOResponse.setOrderId(orderDetailsObj.getOrderId());
+				repository.deleteByOrderId(orderDetailsObj.getOrderId());
+
+			} else {
+				confirmOrderLVOResponse.setOrderId(orderDetailsObj.getOrderId());
+				orderDetailsObj.setHttpStatus(confirmOrderLVOResponse.getStatus());
+				orderDetailsObj.setErrorCode(confirmOrderLVOResponse.getErrorCode());
+				orderDetailsObj.setErrorCategory(confirmOrderLVOResponse.getCategory());
+				orderDetailsObj.setErrorMessage(confirmOrderLVOResponse.getErrorMessage());
+				repository.save(orderDetailsObj);
 			}
-			response  = "MULE API RETURNED :"+200;
-//		}
-			Map<String, ConfirmOrderLVOResponse> re = new ObjectMapper().convertValue(confirmOrderLVOResponse, HashMap.class);
+			String s = "/topic/order-details" + orderDetailsObj1.getSender();
+
+			messagingTemplate.convertAndSend(s, orderDetailsObj);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		response = "MULE API RETURNED :" + 200;
+		// }
+		Map<String, ConfirmOrderLVOResponse> re = new ObjectMapper().convertValue(confirmOrderLVOResponse,
+				HashMap.class);
 
 		return re;
 	}
-	
-//	
-//	@JmsListener(destination = "outbound.queue")
-//	@SendTo("inbound.queue")
-//	public void receiveMessageOnQ2(final Message jsonMessage) throws JMSException {
-//		String messageData = null;
-//		System.out.println("Received message " + jsonMessage);
-//		
-//		String response = null;
-//		if(jsonMessage instanceof TextMessage) {
-//			TextMessage textMessage = (TextMessage)jsonMessage;
-//			messageData = textMessage.getText();
-//			OrderDetails orderDetailsObj = new Gson().fromJson(messageData, OrderDetails.class);
-//			//WRITING TO MONOGO DB
-//			repository.save(orderDetailsObj);
-//		}
-//	}
-	
-//	@SendTo("outbound.queue")
-//	public void send(String destination, String message) {
-//	    jmsTemplate.convertAndSend(destination, message);
-//	  }
-	
-	
-//	@JmsListener(destination = "inbound.topic")
-//	@SendTo("outbound.topic")
-//	public String receiveMessageFromTopic(final Message jsonMessage) throws JMSException {
-//		String messageData = null;
-//		System.out.println("Received message " + jsonMessage);
-//		String response = null;
-//		if(jsonMessage instanceof TextMessage) {
-//			TextMessage textMessage = (TextMessage)jsonMessage;
-//			messageData = textMessage.getText();
-//			Map map = new Gson().fromJson(messageData, Map.class);
-//			response  = "Hello " + map.get("name");
-//		}
-//		return response;
-//	}
 
 }
